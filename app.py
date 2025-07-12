fd33da92203cef03b98d60fa3840d65639c7b738
ChrissyAFK <cshen0800@icloud.com>
2025-07-12 10:35:02 -0700

    initialization of flask

---
+++ app.py
@@ -0,0 +1,13 @@
+import flask
+from flask import Blueprint, render_template, request, redirect, url_for
+
+app = flask.Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/stats')
+def about():
+    return render_template('stats.html')
+@app.route('/support')
+def support():
+    return render_template('support.html')
